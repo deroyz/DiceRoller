740853e6eca52e7d7f6fe037c2eb0d56e4148e71
Young <deroyz@hotmail.com>
2022-06-06 13:32:10 -0600

    Step.05-lateInit variable declaration

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -9,16 +9,21 @@ import android.widget.Toast
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
+
         rollButton.setOnClickListener {
             Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
             rollDice()
         }
 
+        diceImage = findViewById(R.id.dice_image)
 
     }
 
@@ -37,7 +42,6 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(drawableResource)
 
         resultText.text = randomInt.toString()
